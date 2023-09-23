212ce13c625aec6ec328d99578a0d377028591b6
goku <wkurts247@gmail.com>
2023-09-23 12:51:09 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -20,19 +20,51 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all = Bakery.query.all()
+    bakery_dict = all.to_dict()
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    specific = Bakery.query.filter_by(id=id).first()
+    bakery_dict = specific.to_dict()
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict() for bg in baked_goods]
+    response = make_response(
+        jsonify(baked_goods_list),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive is not None:
+        most_expensive_dict = most_expensive.to_dict()
+        response = make_response(
+            jsonify(most_expensive_dict),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        return make_response(jsonify({'message': 'No baked goods found'}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
